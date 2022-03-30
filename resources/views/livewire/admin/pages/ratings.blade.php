4b20107f8965264e01b0503035f24f815c7a9ddf
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-03-30 16:01:54 +0800

    addded

--- resources/views/livewire/admin/pages/ratings.blade.php
+++ resources/views/livewire/admin/pages/ratings.blade.php
@@ -3,9 +3,9 @@
         {{ __('Ratings') }}
     </x-slot>
 
-    <div class="container bg-gradient-to-b from-blue-500 via-blue-500 to-transparent rounded-xl p-10">
+    <div class="container p-10 bg-gradient-to-b from-blue-500 via-blue-500 to-transparent rounded-xl">
        
-            <div class="h-screen-80 md:h-screen-30 w-auto text-indigo-700">
+            <div class="w-auto text-indigo-700 h-screen-80 md:h-screen-30">
                 <livewire:livewire-pie-chart :pie-chart-model="$pieChartModel"/>
             </div>
        
