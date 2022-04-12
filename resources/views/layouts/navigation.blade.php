6f2e7eb2b67cdf9c1a8b6539cfea0929d6a022d2
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-04-12 21:19:30 +0800

    nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,6 +25,14 @@
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="w-6 h-auto text-white fill-current md:hidden"><path d="M6,24H17v2H6a3,3,0,0,1-3-3V7A3,3,0,0,1,6,4H8V2h2V7H8V6H6A1,1,0,0,0,5,7V8H23V7a1,1,0,0,0-1-1H21V4h1a3,3,0,0,1,3,3V18H23V10H5V23A1,1,0,0,0,6,24ZM18,7h2V2H18V4H11V6h7Zm7,17V20H23v4H19v2h4v4h2V26h4V24Z" data-name="31  Calendar,add, Essential, Schedule,add"/></svg>
                         </x-nav-link>
                         @endif
+                        @if (auth()->user()->user_type_id == 1)
+                        <x-nav-link :href="route('ratings')" :active="request()->routeIs('ratings') " class="">
+                            <span class="hidden md:flex">{{ __('Ratings') }}</span>                            
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-auto text-white fill-current md:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                              </svg>
+                        </x-nav-link>
+                        @endif
                         
                     </div>
                 </div>
