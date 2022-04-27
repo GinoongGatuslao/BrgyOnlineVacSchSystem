212a41b779e7d7fe24620a251182439a58dd13fe
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-04-27 16:40:28 +0800

    Revert "Revert "Revert "ports"""
    This reverts commit 08be5ba077d960f9d6ed4d567457843bcca6eb93.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,7 +25,7 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
-    wsPort: 443,
+    wsPort: 6001,
     forceTLS: false,
     disableStats: false,
     
