e4be9d4ea993eadd4c87a75a2db1c3bb79d527ff
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-05-07 22:01:49 +0800

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => "daily",
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 3,
         ],
 
         'slack' => [
